ca95a4b6559c0063e407bce9fc9580bb256d4060
Lars Pötter <justanotherone@gmx.net>
2015-06-05 22:00:17 +0200

    logging with logback and Level debug.

--- src/org/Transformer/Main.java
+++ src/org/Transformer/Main.java
@@ -17,10 +17,17 @@
  */
 package org.Transformer;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
 
 /** Main Class of DataSet Transformer Project.
  * parses the command line and then starts Job Processing or the Wizard(GUI).
@@ -36,6 +43,45 @@ public final class Main
      */
     private Main()
     {
+    }
+
+    public static void setLogLevel(String LogLevel)
+    {
+        final LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        try
+        {
+            final JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(context);
+            context.reset();
+            final String logCfg =
+            "<configuration>" +
+              "<appender name='STDOUT' class='ch.qos.logback.core.ConsoleAppender'>" +
+                "<encoder>" +
+                  "<pattern>%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n</pattern>" +
+                "</encoder>" +
+              "</appender>" +
+              "<root level='" + LogLevel + "'>" +
+                "<appender-ref ref='STDOUT' />" +
+              "</root>" +
+            "</configuration>";
+            ByteArrayInputStream bin;
+            try
+            {
+                bin = new ByteArrayInputStream(logCfg.getBytes("UTF-8"));
+                configurator.doConfigure(bin);
+            }
+            catch(UnsupportedEncodingException e)
+            {
+                // A system without UTF-8 ? - No chance to do anything !
+                e.printStackTrace();
+                System.exit(1);
+            }
+        }
+        catch (JoranException je)
+        {
+          // StatusPrinter will handle this
+        }
+        StatusPrinter.printInCaseOfErrorsOrWarnings(context);
     }
 
     /** Executes the Jobs specified in the Parameter.
@@ -76,7 +122,7 @@ public final class Main
     public static void main(final String[] args)
     {
         final Logger logger = LoggerFactory.getLogger(Main.class);
-
+        setLogLevel("debug");
         if(0 < args.length)
         {
             execute(args[0], logger);
